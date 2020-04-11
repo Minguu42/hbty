58d11588c79cc692002aa6e12cae1e2d0f08e703
min <minguu42@gmail.com>
2020-04-11 14:15:49 +0900

    receive command line argument

--- hbty/core.py
+++ hbty/core.py
@@ -2,11 +2,15 @@ import click
 
 
 @click.command()
-def cli():
+@click.option('--message', '-m', help='バースデーカードに乗せるメッセージ')
+@click.option('-to', default='you', show_default=True,
+              help='Happy Birthday to の後に入れる文字')
+@click.argument('keyword')
+def cli(keyword, message, to):
     """バースデーカード作成ツール"""
-    birthday_card_generate()
+    birthday_card_generate(keyword, message, to)
     click.echo('動作確認')
 
 
-def birthday_card_generate():
+def birthday_card_generate(keyword, message, to):
     pass
